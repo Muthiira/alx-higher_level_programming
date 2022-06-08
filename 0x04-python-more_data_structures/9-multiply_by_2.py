4d82a782e12d229ff7e0f55cd6f89a6bad10f592
Muthiira <calebmuchiri48@gmail.com>
2022-06-08 17:06:03 +0300

    dictionaries

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from re import A
+
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_d = {}
+    for i in a_dictionary:
+        new_d[i] = a_dictionary[i] * 2
+    return new_d
