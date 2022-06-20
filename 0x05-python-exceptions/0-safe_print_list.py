5e96934367b1701f496eda3c1583c97bf52f0a1a
Muthiira <calebmuchiri48@gmail.com>
2022-06-20 16:19:00 +0300

    safe_print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+from re import A
+
+
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except:
+        except A:
             pass
     print()
     return count
